d9861ccba97223efa7575e7799d557cd85574730
umairmohd8 <mohammed08@protonmail.com>
2020-11-14 13:19:08 +0530

    new update

--- attend.py
+++ attend.py
@@ -7,7 +7,7 @@ CONSUMER_SECRET = vars.apisecret
 ACCESS_KEY = vars.Accesstoken
 ACCESS_SECRET = vars.Accesstokensecret
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)  
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
@@ -23,32 +23,36 @@ def login():
         '#intro > div > div > div:nth-child(2) > section > footer > button').click()
 # logs into my account
 
-def store_last_dict(last_attend, file_name):
+def store_last_dict(last_attend, file_name):  #for storing currrent attendace data
     f_write = open(file_name, 'w')
     f_write.write(str(last_attend))
     f_write.close()
     return
 
-def retrieve_last_dict(file_name):
+def retrieve_last_dict(file_name): #for retriving past attendace data
     f_read = open(file_name, 'r')
     lastDic = eval(str(f_read.read().strip()))
     f_read.close()
     return lastDic
 
-def tweet(out,absent):
+def tweet(out,absent):  #for messaging the attendace data to my profile
     api.send_direct_message(3250564195,out)
     api.send_direct_message(3250564195,absent)
 
-def table(fin):
-    fin2 = retrieve_last_dict('last_dict.txt')
+def table(fin):    #creating the table for attendace data
+    finOld = retrieve_last_dict('last_dict.txt')
     out = "{:<8} {:<8} {:<8} {:<8}\n".format('Subject','Held','present','percent')
-    absent = 'You were absent for '
-    for (k1,v1), (k2,v2) in zip(fin.items(),fin2.items()):
-        h1, a1, percent = v1    #h1, a1 are current week held and attended classes
-        h2, a2, _ =v2            #h2, a2 are past week held and attended classes
+    a = 'You were absent for '
+    absent = a
+    for (k1,v1), (k2,v2) in zip(finOld.items(),fin.items()):
+        h1, a1, percent = v1    #h1, a1 are past week held and attended classes
+        h2, a2, _ =v2            #h2, a2 are current week held and attended classes
+        not_present = (h2-h1) - (a2-a1)    # number of absent classes
         out += "{:<8} {:<8} {:<8} {:<8}\n".format(k1,h1,a1,percent)
-        if h2 > h1 and a2 < h2 and a1 == a2:       #prints the absent classes
-            absent += "{cls} of {sub}, ".format(cls = h2-h1,sub = k1)
+        if not_present:       #prints the absent classes
+            absent += "{cls} of {sub}, ".format(cls = not_present,sub = k1)
+    if absent == a: #if absent for no classes
+        absent = a + 'no classes.'
     tweet(out,absent)
     print(out)        
     print(absent)
